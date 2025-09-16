19c33e96bda1f93449427d0aa6c822d6be611673
Shahzaib Awan <awan.shahzaib.0314@gmail.com>
2025-09-16 15:45:43 +0500

    update pics

--- src/app/components/Three.jsx
+++ src/app/components/Three.jsx
@@ -45,7 +45,7 @@ export default function Three() {
             </div>
 
             <h3 className="mt-5 text-lg font-bold text-black">{step.title}</h3>
-            <p className="mt-2 text-sm text-gray-500 max-w-[200px]">
+            <p className="mt-3 text-sm text-gray-500 max-w-[200px]">
               {step.desc}
             </p>
           </div>
